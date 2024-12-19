2b2cf347e955bc5ded46becbecb188000cdd583c
GLeonBS <gabriel.leonbs@gmail.com>
2024-12-19 14:17:57 -0300

    feat: 🎸 Add controllers

--- src/main/java/com/petland/PetlandApiApplication.java
+++ src/main/java/com/petland/PetlandApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PetlandApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetlandApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PetlandApiApplication.class, args);
+    }
 
 }
--- src/main/java/com/petland/start/StartApp.java
+++ src/main/java/com/petland/start/StartApp.java
@@ -1,15 +1,10 @@
 package com.petland.start;
 
-import com.petland.model.Animal;
-import com.petland.model.AnimalEspecie;
-import com.petland.model.ProdutoServico;
 import com.petland.repository.AnimalRepository;
 import com.petland.repository.ProdutoServicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
 
 @Component
 public class StartApp implements ApplicationRunner {
@@ -23,17 +18,17 @@ public class StartApp implements ApplicationRunner {
 
     @Override
     public void run(org.springframework.boot.ApplicationArguments args) throws Exception {
-        Animal bolota = new Animal();
-        bolota.setNome("Bolota");
-        bolota.setEspecie(AnimalEspecie.CACHORRO);
-        bolota.setAniversario(LocalDate.of(2018, 1, 1));
-
-        ProdutoServico banhoETosa = new ProdutoServico();
-        banhoETosa.setNome("Banho e Tosa");
-        banhoETosa.setValor(60.0);
-        banhoETosa.setServico(true);
-
-        animalRepository.save(bolota);
-        produtoServicoRepository.save(banhoETosa);
+        //        Animal bolota = new Animal();
+        //        bolota.setNome("Bolota");
+        //        bolota.setEspecie(AnimalEspecie.CACHORRO);
+        //        bolota.setAniversario(LocalDate.of(2018, 1, 1));
+        //
+        //        ProdutoServico banhoETosa = new ProdutoServico();
+        //        banhoETosa.setNome("Banho e Tosa");
+        //        banhoETosa.setValor(60.0);
+        //        banhoETosa.setServico(true);
+        //
+        //        animalRepository.save(bolota);
+        //        produtoServicoRepository.save(banhoETosa);
     }
 }
